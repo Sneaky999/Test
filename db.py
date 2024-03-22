c18eeb07b5b98a277eb836486de77d9eb7131b50
Sneaky999 <132036064+Sneaky999@users.noreply.github.com>
2024-03-22 21:35:02 +0545

    Add files via upload

---
+++ db.py
@@ -0,0 +1,18 @@
+db = {}
+print('Welcome to the simplest key-value database')
+while True:
+    print('What do you want to do?')
+    print('Enter P to [P]ut, G to [G]et ')
+    action = input()
+    if action == 'P':
+        k = input('Enter key: ')
+        d = input('Enter data: ')
+        db[k] = d
+    elif action == 'G':
+        k = input('Enter key: ')
+        if not k in db:
+            print('No such key')
+        else:
+            print('Your data: %s' % db[k])
+  
+  
